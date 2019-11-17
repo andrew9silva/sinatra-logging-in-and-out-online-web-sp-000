f7c9096849dfe5fae1196f0138fa0bec1e152d1e
Andrew Silva <andrewsilva9@gmail.com>
2019-11-17 20:37:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,9 @@ class ApplicationController < Sinatra::Base
     if @user 
       session[user_id] = @user_id
       redirect '/account'
-      erb :
+      erb :account
+    else
+      
     end
 
   end
