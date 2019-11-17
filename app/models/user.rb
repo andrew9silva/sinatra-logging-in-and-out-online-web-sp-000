6a9470d116e4975681b0f4a0529416a03c348d93
Andrew Silva <andrewsilva9@gmail.com>
2019-11-17 21:00:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
   
   attr_accessor :username, :password, :balance 
   
-  def initialize(username, password, balance)
-    @username = username
-    @password = password
-    @balance = balance
-  end
+ 
 end
 
